b009eadb93cd58df26375e07d66c99b7b9c2e1d8
Róbert Fjölnir Birkisson <rfb97@hotmail.com>
2023-02-26 20:06:53 +0100

    Added React. to prompt

--- src/repositories/generate-logic.js
+++ src/repositories/generate-logic.js
@@ -11,7 +11,7 @@ export const generateComponentFromPrompt = async (prompt) => {
     const openai = new OpenAIApi(config);
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: `Create a React component with tailwind using the following prompt: "${prompt}" Make the generated component as an anonymous arrow function that doesn't have a name so it is instantly invoked`,
+        prompt: `Create a React component with tailwind using the following prompt: "${prompt}" Make the generated component as an anonymous arrow function that doesn't have a name so it is instantly invoked and also when using React specific functions, append 'React.' to the front.`,
         temperature: 0,
         max_tokens: 250,
         frequency_penalty: 0.25,
