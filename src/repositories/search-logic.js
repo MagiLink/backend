a1ede07dee81942e4044901d11c67210f329fbb1
gasparyanartur <gasparyanartur99@gmail.com>
2023-02-27 20:02:02 +0100

    Add mock response to /search/

--- src/repositories/search-logic.js
+++ src/repositories/search-logic.js
@@ -6,12 +6,43 @@ const config = new Configuration({
     apiKey: OPENAI_KEY,
 });
 
-export const convertSearchToVector = async (searchString) => {
+export const embedPrompt = async (prompt) => {
     const openai = new OpenAIApi(config);
     const response = await openai.createEmbedding({
         model: "text-embedding-ada-002",
-        input: searchString,
+        input: prompt,
     });
 
-    return response.data.data[0].embedding
+    return response.data.data[0].embedding;
 };
+
+export const getTopComponents = async (prompt, topK) => {
+    // TODO: Implement query logic here.
+    // TODO: integrate with Redis vector similarity search
+
+    return getMockComponents(prompt, topK)
+}
+
+
+function getMockComponents(prompt, topK) {
+    const exampleComponents = [];
+    for (let i = 0; i < 100; i++) {
+        exampleComponents.push(`example code nr {i+1}`);
+    }
+
+    const matches = [];
+    for (let i = 0; i < topK; i++) {
+        const randomIndex = Math.random() * exampleComponents.length;
+        const component = exampleComponents.splice(randomIndex, 1);
+
+        const match = {
+            "prompt": "example prompt",
+            "component": component,
+            "score": Math.random()
+        };
+
+        matches.push(match);
+    }
+
+    return matches;
+}
--- src/routes/search.js
+++ src/routes/search.js
@@ -1,18 +1,28 @@
 import express from 'express';
-import { convertSearchToVector } from '../repositories/search-logic.js';
+import { embedPrompt, getTopComponents } from '../repositories/search-logic.js';
 
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
-    const searchString = req.body.search
+    /*
+     *  Expects JSON packet with following:
+     *      "prompt": Raw string representing a search.
+     *      "top_k": Expected number of returned component-score pairs, sorted by score.
+     * 
+     *  Returns a sorted list of JSON packets containing:
+     *      "embedding": Embedded prompt corresponding to this compoment.
+     *      "component": Text string containing JavaScript code matching the prompt.
+     *      "score": Number representing how closely this prompt matches the given one.
+     */
 
-    // We get a high dimensional representation of our search string
-    const searchVector = await convertSearchToVector(searchString);
+    const prompt = req.body["prompt"];
+    const topK = req.body["top_k"];
 
+    const embedding = await embedPrompt(prompt);
+    const matches = await getTopComponents(embedding, topK);
 
-    // TODO: integrate with Redis vector similarity search
-
-    res.send('Blocked until Redis is ready!')
+    res.send({'matches': matches});
 });
+
 
 export default router;
