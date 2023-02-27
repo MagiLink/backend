37f65ca6c22708da619a0015936669444e357060
Bence Gadanyi <29914644+Beenyaa@users.noreply.github.com>
2023-02-27 19:41:52 +0000

    feat: moves redis secrets to .env

--- src/routes/query.js
+++ src/routes/query.js
@@ -1,24 +1,23 @@
 import express from 'express';
-import { Configuration, OpenAIApi } from 'openai'
-import { createClient, SchemaFieldTypes, VectorAlgorithms } from 'redis'
+import { Configuration, OpenAIApi } from 'openai';
+import { createClient, SchemaFieldTypes, VectorAlgorithms } from 'redis';
 const OPENAI_KEY = process.env.OPENAI_KEY;
 const configuration = new Configuration({
     apiKey: OPENAI_KEY,
 });
 
-
-const REDISHOST = 'containers-us-west-86.railway.app'
-const REDISPASSWORD = 'QSWJKohcVxwUnB2bKJyr'
-const REDISPORT = '6947'
-const REDISUSER = 'default'
-
+const REDISHOST = process.env.REDIS_HOST;
+const REDISPASSWORD = process.env.REDIS_PASSWORD;
+const REDISPORT = process.env.REDIS_PORT;
+const REDISUSER = process.env.REDIS_USER;
+const REDISURL = `redis://${REDISUSER}:${REDISPASSWORD}@${REDISHOST}:${REDISPORT}`;
 
 const router = express.Router();
 const client = createClient({
-    url: `redis://${ REDISUSER }:${ REDISPASSWORD }@${ REDISHOST }:${ REDISPORT }`
+    url: REDISURL,
 });
 
-const EMBEDDING_DIM = 2;          // TODO: Ask how to make global constants
+const EMBEDDING_DIM = 2; // TODO: Ask how to make global constants
 const queryString = '*=>[KNN 4 @v $BLOB AS dist]';
 
 function float32Buffer(arr) {
@@ -26,23 +25,27 @@ function float32Buffer(arr) {
 }
 
 // Should we perhaps export the init function and call it externally?
-async function init() { 
-    await client.connect();         
+async function init() {
+    await client.connect();
 
     try {
-        await client.ft.create('idx:component-db', {
-            prompt: {
-                type: SchemaFieldTypes.VECTOR,
-                ALGORITHM: VectorAlgorithms.HNSW,       // https://www.pinecone.io/learn/hnsw/
-                TYPE: 'FLOAT32',
-                DIM: EMBEDDING_DIM,
-                DISTANCE_METRIC: 'COSINE'
+        await client.ft.create(
+            'idx:component-db',
+            {
+                prompt: {
+                    type: SchemaFieldTypes.VECTOR,
+                    ALGORITHM: VectorAlgorithms.HNSW, // https://www.pinecone.io/learn/hnsw/
+                    TYPE: 'FLOAT32',
+                    DIM: EMBEDDING_DIM,
+                    DISTANCE_METRIC: 'COSINE',
+                },
+                code: SchemaFieldTypes.TEXT,
             },
-            code: SchemaFieldTypes.TEXT
-        }, {
-            ON: 'HASH',
-            PREFIX:'magilink:component'
-        });
+            {
+                ON: 'HASH',
+                PREFIX: 'magilink:component',
+            },
+        );
     } catch (e) {
         if (e.message === 'Index already exists') {
             console.log('Index exists already, skipped creation.');
@@ -56,10 +59,8 @@ async function init() {
         client.hSet('magilink:component:a', { prompt: float32Buffer([0.1, 0.1]) }),
         client.hSet('magilink:component:b', { prompt: float32Buffer([0.1, 0.4]) }),
         client.hSet('magilink:component:c', { prompt: float32Buffer([0.1, 0.6]) }),
-        client.hSet('magilink:component:d', { prompt: float32Buffer([0.1, 0.8]) })
+        client.hSet('magilink:component:d', { prompt: float32Buffer([0.1, 0.8]) }),
     ]);
-
-    
 }
 
 await init();
@@ -71,22 +72,22 @@ await init();
 router.get('/', async (req, res, next) => {
     const results = await client.ft.search('idx:component-db', queryString, {
         PARAMS: {
-         BLOB: float32Buffer(['0.1', 0.1])
+            BLOB: float32Buffer(['0.1', 0.1]),
         },
         SORTBY: 'dist',
         DIALECT: 2,
-        RETURN: ['dist']
-     });
- 
-     console.log(JSON.stringify(results, null, 2));
-     res.send("OK");
+        RETURN: ['dist'],
+    });
+
+    console.log(JSON.stringify(results, null, 2));
+    res.send('OK');
 });
 
-router.post('/store', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     const prompt = req.body['code'];
     console.log(prompt);
 
-    res.send("OK");
-})
+    res.send('OK');
+});
 
 export default router;
