a313c089690dc4988f497267d0a4f01f26123219
Bence Gadanyi <29914644+Beenyaa@users.noreply.github.com>
2023-02-28 09:40:04 +0000

    feat: swagger documentation of endpoints

--- src/routes/query.js
+++ src/routes/query.js
@@ -1,21 +1,56 @@
 import express from 'express';
-import { addComponentToDatabase,  queryComponentDatabase } from '../repositories/query-logic.js'
-
+import { addComponentToDatabase, queryComponentDatabase } from '../repositories/query-logic.js';
 
 const router = express.Router();
 
 // TODO: Add disconnecting functionality
 
 // https://github.com/redis/node-redis/blob/master/examples/search-knn.js
-addComponentToDatabase('a', [0.02, 0.59], "hello");
-addComponentToDatabase('b', [0.03, 0.14], "my");
-addComponentToDatabase('c', [0.6, 0.4], "name");
+addComponentToDatabase('a', [0.02, 0.59], 'hello');
+addComponentToDatabase('b', [0.03, 0.14], 'my');
+addComponentToDatabase('c', [0.6, 0.4], 'name');
+
+/**
+ * @swagger
+ * /query:
+ *   post:
+ *     summary: Use this to query the database
+ *     requestBody:
+ *       description: Embedded prompt corresponding to this component.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               component:
+ *                 type: string
+ *               score:
+ *                 type: number
+ *     responses:
+ *       '200':
+ *         description: A list of component-score pairs, sorted by score.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 matches:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       component:
+ *                         type: string
+ *                       score:
+ *                         type: number
+ */
 
 router.post('/', async (req, res, next) => {
-   const response = await queryComponentDatabase(req.body['embedding']);
-   console.log(response);
-   res.send(response);
+    console.log('Received request for query');
+    const response = await queryComponentDatabase(req.body['embedding']);
+    console.log('Response from query: ', response);
+    res.send(response);
 });
-
 
 export default router;
