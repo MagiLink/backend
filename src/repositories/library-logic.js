59e7a412ba5891303e99649f52b070f46b973d57
robert-fjolnir <43745091+robert-fjolnir@users.noreply.github.com>
2023-03-02 21:29:54 +0100

    Added delete endpoint for components (#18)

--- src/repositories/library-logic.js
+++ src/repositories/library-logic.js
@@ -111,6 +111,11 @@ const getComponentFromHash = async (hashKey) => {
 }
 
 
+export const deleteComponentWithHash = async (hashKey) => {
+    client.del(hashKey);
+}
+
+
 export const getAllComponents = async () => {
     const hashes = await getAllHashesFromDatabase();
 
