b9f3128255759e916b564087ebb94feb9c780f9e
Bence Gadanyi <29914644+Beenyaa@users.noreply.github.com>
2023-03-02 12:40:34 +0000

    feat: exploring gpt-3.5 for better results

--- src/repositories/generate-logic.js
+++ src/repositories/generate-logic.js
@@ -7,24 +7,24 @@ const config = new Configuration({
 });
 
 export const generateComponentFromPrompt = async (prompt) => {
-    console.log(config.apiKey);
     const openai = new OpenAIApi(config);
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `Create a React component styled with the style prop using the following prompt: "${prompt}" Make the generated component as an anonymous arrow function that doesn't have a name so it is instantly invoked and also when using React specific functions, append 'React.' to the front.`,
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: `ONLY RESPOND WITH CODE, IGNORE ANY IMPORTS AND EXPORTS AND START THE CODE WITH  \`() => {\`: Create a React component styled with the style prop using the following prompt: \`${prompt}\` Make the generated component as an anonymous arrow function that doesn't have a name so it is instantly invoked and also when using React specific functions, append 'React.' to the front.`,
+            },
+        ],
         temperature: 0,
-        max_tokens: 250,
         frequency_penalty: 0.25,
     });
 
-    let returnedCode = response.data.choices[0].text;
+    let returnedCode = response.data.choices[0].message.content;
 
     // Remove dots and newlines from the start of the code
-    while (
-        returnedCode.charAt(0) == '.' ||
-        returnedCode.charAt(0) == ',' || 
-        returnedCode.charAt(0) == '\n') {
-        returnedCode = returnedCode.substring(1)
+    while (returnedCode.charAt(0) == '.' || returnedCode.charAt(0) == ',' || returnedCode.charAt(0) == '\n') {
+        returnedCode = returnedCode.substring(1);
     }
 
     return returnedCode;
